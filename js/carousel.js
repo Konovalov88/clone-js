f4f50307ecac421d16d009429cc8c5e8f9d15981
Konovalov88 <3dkinopark@gmail.com>
2021-11-26 08:40:51 +0000

    Vrode

--- js/carousel.js
+++ js/carousel.js
@@ -1,85 +1,63 @@
 (function() {
-    const slides = [
-        {   
-            id: "1",
-            image: "img/icon-hive.svg",
-            title: "Clover Honey",
-            description: `Thick honey is produced by honeybees that collect the nectar of clover plants. Mild in taste and light in color.`,
-        },
-        {   
-            id: "2",
-            image: "img/icon-drop.svg",
-            title: "Buckwheat Honey",
-            description: `It is highly nutritious amber-colored honey with a slight reddish tint. It has a has a pleasant sweet taste.`,
-        },        
-        {   
-            id: "3",
-            image: "img/icon-garden.svg",
-            title: "Wildflower Honey",
-            description: `It is also known as polyfloral honey. It is made from the nectar of different species of flowers or blossoms.`,
-        },        
-        {   
-            id: "4",
-            image: "img/icon-hive.svg",
-            title: "Citrus Blossom Honey",
-            description: `This honey is sweet and relatively thick, a perfect match for your tea or toast. It is the best association with California!`,
-        },
-    ];
-       
+	const slides = [
 
-function renderSlidesTypesOfHoneyCarousel (typesOfHoneyCarousel) {
-    const slidesContainer = document.querySelector('.types-of-honey-carousel .columns-carousel-types')
-}
+        `<div class="columns-carousel-types">
+            <img class="columns-img" src="img/icon-hive.svg" alt="Icon Hive">
+            <h3 class="columns-title">Clover Honey</h3>
+            <p class="text-columns">Thick honey is produced by honeybees that collect the nectar of clover plants. Mild in taste and light in color.</p>
+            <button class="button">Shop Now</button>
+        </div>`,
 
+        `<div class="columns-carousel-types">
+            <img class="columns-img" src="img/icon-drop.svg" alt="Icon Drop">
+            <h3 class="columns-title">Buckwheat Honey</h3>
+            <p class="text-columns">It is highly nutritious amber-colored honey with a slight reddish tint. It has a has a pleasant sweet taste.</p>
+            <button class="button">Shop Now</button>
+        </div>`,
 
-    const productsContainer = document.querySelector(".products");
-    let html = "";
-    for (const product of sortedProducts) {
-      html += `
-             <article class="product">
-                <img src="${product.image}" alt="${product.title}" />
-                <h3>${product.title}</h3>
-                <p>${product.description}</p>
-                <div class="buttons">
-                    <button class="button card-button">Shop Now</button>
-                </div>
-            </article>`;
-    }
-    productsContainer.innerHTML = html;
-  }
+        `<div class="columns-carousel-types">
+            <img class="columns-img" src="img/icon-garden.svg" alt="Icon Garden">
+            <h3 class="columns-title">Wildflower Honey</h3>
+            <p class="text-columns">It is also known as polyfloral honey. It is made from the nectar of different species of flowers or blossoms.</p>
+            <button class="button">Shop Now</button>
+        </div>`,
 
-    let currentSlideIdx = 0;
+        `<div class="columns-carousel-types">
+            <img class="columns-img" src="img/icon-sweet.svg" alt="Icon Sweet">
+            <h3 class="columns-title">Citrus Blossom Honey</h3>
+            <p class="text-columns">This honey is sweet and relatively thick, a perfect match for your tea or toast. It is the best association with California!</p>
+            <button class="button">Shop Now</button>
+        </div>`
+	];
 
-    function renderSlides() {
-        const slidesContainer = document.querySelector('.carousel .slides-container');
-        slidesContainer.innerHTML = slides[currentSlideIdx];
-        if (window.innerWidth > 500) {
-            const slide2Idx = currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
-            slidesContainer.innerHTML += slides[slide2Idx];
-            if (window.innerWidth > 900) {
-                const slide3Idx = slide2Idx + 1 >= slides.length ? 0 : slide2Idx + 1;
-                slidesContainer.innerHTML += slides[slide3Idx];    
-            }
-        }
-    }
+	let currentSlideIdx = 0;
 
-    function nextSlide() {
-        currentSlideIdx++;
-        if (currentSlideIdx >= slides.length) currentSlideIdx = 0;
-        renderSlides();
-    }
+	function renderSlides() {
+		 const slidesContainer = document.querySelector('.types-of-honey-carousel .types-of-honey-slides');
+		 slidesContainer.innerHTML = slides[currentSlideIdx];
+		 if (window.innerWidth > 500) {
+			  const slide2Idx = currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
+			  slidesContainer.innerHTML += slides[slide2Idx];
+		}
+	}
 
-    function prevSlide() {
-        currentSlideIdx--;
-        if (currentSlideIdx < 0) currentSlideIdx = slides.length - 1;
-        renderSlides();
-    }
+	function nextSlide() {
+		 currentSlideIdx++;
+		 if (currentSlideIdx >= slides.length) currentSlideIdx = 0;
+		 renderSlides();
+	}
 
-    
-    document.querySelector('.carousel .prev').addEventListener('click', prevSlide);
-    document.querySelector('.carousel .next').addEventListener('click', nextSlide);
+	function prevSlide() {
+		 currentSlideIdx--;
+		 if (currentSlideIdx < 0) currentSlideIdx = slides.length - 1;
+		 renderSlides();
+	}
 
-    renderSlides();
+	setInterval(nextSlide, 60000);
+	document.querySelector('.prev').addEventListener('click', prevSlide);
+	document.querySelector('.next').addEventListener('click', nextSlide);
 
-    window.addEventListener('resize', renderSlides);
+	renderSlides();
+
+	window.addEventListener('resize', renderSlides);
 })();
